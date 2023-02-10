aec7deb28793bcf786d3f415fb64669412646fe6
Ivan Klimovskiy <klimoffsky2707@yandex.ru>
2023-02-11 01:48:40 +0300

    Добавить адаптив кроме раздела Наше производство

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,14 @@
 import "./index.css";
+
+const burgerMenuButton = document.querySelector(".nav-menu__burger-menu");
+const smallMenuOverlay = document.querySelector(".nav-menu-small__overlay");
+const smallNavMenu = document.querySelector(".nav-menu-small");
+const buttonCloseSmallNavMenu = document.querySelector(".nav-menu-small__close-button");
+burgerMenuButton.addEventListener("click", () => {
+  smallMenuOverlay.classList.add("nav-menu-small__overlay_active");
+  smallNavMenu.classList.add("nav-menu-small_active");
+})
+buttonCloseSmallNavMenu.addEventListener("click", () => {
+  smallMenuOverlay.classList.remove("nav-menu-small__overlay_active");
+  smallNavMenu.classList.remove("nav-menu-small_active");
+})
