9eb54fa30f9b3797fb5a69f6f12f5f70776280f1
Ivan Klimovskiy <klimoffsky2707@yandex.ru>
2023-09-13 15:12:52 +0300

    Добавить кнопку закрытия баннера и исправить баги с кнопкой в начало

--- src/components/variables.js
+++ src/components/variables.js
@@ -1,64 +1,38 @@
-const burgerMenuButton = document.querySelector(".nav-menu__burger-menu");
-const smallMenuOverlay = document.querySelector(".nav-menu-small__overlay");
-const smallNavMenu = document.querySelector(".nav-menu-small");
-const buttonCloseSmallNavMenu = document.querySelector(
-  ".nav-menu-small__close-button"
-);
+const burgerMenuButton = document.querySelector('.nav-menu__burger-menu');
+const smallMenuOverlay = document.querySelector('.nav-menu-small__overlay');
+const smallNavMenu = document.querySelector('.nav-menu-small');
+const navMenu = document.querySelector('.nav-menu');
+const buttonCloseSmallNavMenu = document.querySelector('.nav-menu-small__close-button');
 const buttonToUp = document.querySelector('.button-to-top');
-
+const buttonCloseBanner = document.querySelector('.tinkoff-banner__close-button');
 const tinkoffBanner = document.querySelector('.tinkoff-banner');
 const buttonOpenTinkoff = document.querySelector('.tinkoff-banner__button');
-const popupWithImage = document.querySelector(".popup_type_image");
-const popupOpenedImage = document.querySelector(".popup__image");
-const popupWithForm = document.querySelector(".popup_type_form");
+const popupWithImage = document.querySelector('.popup_type_image');
+const popupOpenedImage = document.querySelector('.popup__image');
+const popupWithForm = document.querySelector('.popup_type_form');
 const popupTinkoff = document.querySelector('.popup_type_tinkoff-banner');
-const buttonsOpenForm = Array.from(
-  document.querySelectorAll(".button_type_popup")
-);
-const buttonsClosePopup = Array.from(
-  document.querySelectorAll(".popup__close-button")
-);
-const popupList = Array.from(document.querySelectorAll(".popup"));
-const popupTitle = document.querySelector(".popup__title");
-const chatButton = document.querySelector(".messenger__button");
-const chatLinks = document.querySelector(".messenger__links");
-const overlay = document.querySelector("#overlay");
-const messenger = document.querySelector(".messenger");
-const messengerLinks = Array.from(
-  document.querySelectorAll(".messenger__link")
-);
-const galleryPhotoElements = Array.from(
-  document.querySelectorAll(".gallery__main-item")
-);
-const nextSlide = document.querySelector(
-  ".our-production__slider-button_type_next"
-);
-const prevSlide = document.querySelector(
-  ".our-production__slider-button_type_prev"
-);
-const sliderWrapper = document.querySelector(".our-production__slider-wrapper");
-
-const slidesNumber = document.querySelector(
-  ".our-production__slider-wrapper"
-).childElementCount;
-const buttonConfirmation = document.querySelector(
-  ".popup__button-confirmation"
-);
-const popupConfirmSentForm = document.querySelector(
-  ".popup_type_send-form-confirmation"
-);
-const subtitleSendFormConfirmation = document.querySelector(
-  ".popup__subtitle_type_send-form-confirmation"
-);
+const buttonsOpenForm = Array.from(document.querySelectorAll('.button_type_popup'));
+const buttonsClosePopup = Array.from(document.querySelectorAll('.popup__close-button'));
+const popupList = Array.from(document.querySelectorAll('.popup'));
+const popupTitle = document.querySelector('.popup__title');
+const chatButton = document.querySelector('.messenger__button');
+const chatLinks = document.querySelector('.messenger__links');
+const overlay = document.querySelector('#overlay');
+const messenger = document.querySelector('.messenger');
+const messengerLinks = Array.from(document.querySelectorAll('.messenger__link'));
+const galleryPhotoElements = Array.from(document.querySelectorAll('.gallery__main-item'));
+const nextSlide = document.querySelector('.our-production__slider-button_type_next');
+const prevSlide = document.querySelector('.our-production__slider-button_type_prev');
+const sliderWrapper = document.querySelector('.our-production__slider-wrapper');
+const slidesNumber = document.querySelector('.our-production__slider-wrapper').childElementCount;
+const buttonConfirmation = document.querySelector('.popup__button-confirmation');
+const popupConfirmSentForm = document.querySelector('.popup_type_send-form-confirmation');
+const subtitleSendFormConfirmation = document.querySelector('.popup__subtitle_type_send-form-confirmation');
 const forms = Array.from(document.forms);
-const agreementCheckboxes = Array.from(
-  document.querySelectorAll("input[name=agreement]")
-);
-const root = document.querySelector(".root");
-const smallMenuLinks = Array.from(
-  document.querySelectorAll(".nav-menu-small__item-link")
-);
-const formOpenedPopup = document.querySelector(".form_type_popup");
+const agreementCheckboxes = Array.from(document.querySelectorAll('input[name=agreement]'));
+const root = document.querySelector('.root');
+const smallMenuLinks = Array.from(document.querySelectorAll('.nav-menu-small__item-link'));
+const formOpenedPopup = document.querySelector('.form_type_popup');
 
 export {
   burgerMenuButton,
@@ -93,5 +67,7 @@ export {
   buttonOpenTinkoff,
   popupTinkoff,
   tinkoffBanner,
-  buttonToUp
+  buttonToUp,
+  buttonCloseBanner,
+  navMenu,
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import "./index.css";
+import './index.css';
 import {
   burgerMenuButton,
   smallMenuOverlay,
@@ -27,77 +27,75 @@ import {
   smallMenuLinks,
   slidesNumber,
   root,
-  buttonToUp
-} from "../components/variables";
-import {
-  closePopup,
-  closeSmallMenu,
-  openPopup,
-  openSmallMenu,
-} from "../components/modal";
+  buttonToUp,
+  buttonCloseBanner,
+  tinkoffBanner,
+  navMenu,
+} from '../components/variables';
+import { closePopup, closeSmallMenu, openPopup, openSmallMenu } from '../components/modal';
 import {
   activateSubmitButton,
   deactivateSubmitButton,
   enableValidation,
   hasInvalidInputs,
   toggleButtonSendingData,
-} from "../components/validation";
-import maskPhone from "../components/mask";
-import {unlockScroll} from "../components/utils";
+} from '../components/validation';
+import maskPhone from '../components/mask';
+import { unlockScroll } from '../components/utils';
 
 export const settings = {
-  formElement: ".form",
-  inputElement: ".form__input",
-  submitButtonSelector: ".form__submit",
-  inactiveButtonClass: "form__submit_disabled",
-  inputErrorClass: "form__input_type_error",
-  errorClass: ".form__input-error",
+  formElement: '.form',
+  inputElement: '.form__input',
+  submitButtonSelector: '.form__submit',
+  inactiveButtonClass: 'form__submit_disabled',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: '.form__input-error',
 };
 
-export let scrollWidth = window.innerWidth - root.offsetWidth + "px";
+export let scrollWidth = window.innerWidth - root.offsetWidth + 'px';
 let slideWidthCounter = 0;
-let slideWidth = document.querySelector(".our-production__slider-image").offsetWidth;
+let slideWidth = document.querySelector('.our-production__slider-image').offsetWidth;
 
 //Слушатель на бургер меню
-burgerMenuButton.addEventListener("click", openSmallMenu);
-buttonCloseSmallNavMenu.addEventListener("click", closeSmallMenu);
-smallMenuOverlay.addEventListener("click", (evt) => {
-  if (evt.target.classList.contains("nav-menu-small__overlay")) {
+burgerMenuButton.addEventListener('click', openSmallMenu);
+buttonCloseSmallNavMenu.addEventListener('click', closeSmallMenu);
+smallMenuOverlay.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('nav-menu-small__overlay')) {
     closeSmallMenu();
   }
 });
 
 // Слушатель на все кнопки открытия попапа и замены текста заголовка
 buttonsOpenForm.forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener('click', () => {
     popupTitle.textContent = button.textContent;
     openPopup(popupWithForm);
   });
 });
 buttonOpenTinkoff.addEventListener('click', function () {
-  openPopup(popupTinkoff)
-})
+  openPopup(popupTinkoff);
+});
 // Слушатели для иконки мессенджера и оверлея
 messengerLinks.forEach((messengerLink) => {
-  messengerLink.addEventListener("click", () => {
-    chatLinks.classList.remove("messenger__links_active");
-    overlay.style.display = "none";
+  messengerLink.addEventListener('click', () => {
+    chatLinks.classList.remove('messenger__links_active');
+    overlay.style.display = 'none';
   });
 });
-document.addEventListener("click", function (evt) {
-  if (evt.target.id === "overlay") {
-    chatLinks.classList.remove("messenger__links_active");
-    overlay.style.display = "none";
+document.addEventListener('click', function (evt) {
+  if (evt.target.id === 'overlay') {
+    chatLinks.classList.remove('messenger__links_active');
+    overlay.style.display = 'none';
   }
   if (evt.target === chatButton) {
-    chatLinks.classList.toggle("messenger__links_active");
-    overlay.style.display = "block";
+    chatLinks.classList.toggle('messenger__links_active');
+    overlay.style.display = 'block';
   }
 });
 
 // Слушатель на открытия попапа с картинкой
 galleryPhotoElements.forEach((element) => {
-  element.addEventListener("click", (evt) => {
+  element.addEventListener('click', (evt) => {
     openPopup(popupWithImage);
     popupOpenedImage.src = evt.target.src;
     popupOpenedImage.alt = evt.target.alt;
@@ -105,7 +103,7 @@ galleryPhotoElements.forEach((element) => {
 });
 
 // Слайдер
-nextSlide.addEventListener("click", () => {
+nextSlide.addEventListener('click', () => {
   nextSlide.disabled = true;
   slideWidthCounter += slideWidth;
   sliderWrapper.style.transform = `translateX(-${slideWidthCounter}px)`;
@@ -118,14 +116,12 @@ nextSlide.addEventListener("click", () => {
   }, 1000);
 });
 
-prevSlide.addEventListener("click", () => {
+prevSlide.addEventListener('click', () => {
   prevSlide.disabled = true;
   slideWidthCounter -= slideWidth;
   sliderWrapper.style.transform = `translateX(-${slideWidthCounter}px)`;
   if (slideWidthCounter === -slideWidth) {
-    sliderWrapper.style.transform = `translateX(-${
-      slideWidth * (slidesNumber - 1)
-    }px)`;
+    sliderWrapper.style.transform = `translateX(-${slideWidth * (slidesNumber - 1)}px)`;
     slideWidthCounter = slideWidth * (slidesNumber - 1);
   }
   setTimeout(() => {
@@ -133,16 +129,16 @@ prevSlide.addEventListener("click", () => {
   }, 1000);
 });
 
-buttonConfirmation.addEventListener("click", () => {
+buttonConfirmation.addEventListener('click', () => {
   closePopup(popupConfirmSentForm);
 });
 
 forms.forEach((form) => {
-  form.addEventListener("submit", (evt) => {
+  form.addEventListener('submit', (evt) => {
     evt.preventDefault();
     toggleButtonSendingData(false, evt);
     const formData = new FormData(form);
-    fetch("sendmail.php", {method: "POST", body: formData})
+    fetch('sendmail.php', { method: 'POST', body: formData })
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -152,17 +148,17 @@ forms.forEach((form) => {
       })
       .then((response) => {
         subtitleSendFormConfirmation.textContent = response.message;
-        closePopup(popupWithForm)
+        closePopup(popupWithForm);
       })
       .catch((error) => {
         subtitleSendFormConfirmation.textContent = error.message;
-        closePopup(popupWithForm)
+        closePopup(popupWithForm);
       })
       .finally(() => {
         toggleButtonSendingData(true, evt);
         setTimeout(() => {
           openPopup(popupConfirmSentForm);
-        }, 200)
+        }, 200);
       });
     form.reset();
     deactivateSubmitButton(form);
@@ -172,17 +168,10 @@ forms.forEach((form) => {
 enableValidation(settings);
 
 agreementCheckboxes.forEach((checkbox) => {
-  checkbox.addEventListener("change", (evt) => {
-    const closestForm = evt.target.closest(".form");
-    const agreementCheckbox = closestForm.querySelector(
-      "input[name=agreement]"
-    );
-    if (
-      hasInvalidInputs(
-        Array.from(closestForm.querySelectorAll(".form__input"))
-      ) ||
-      !agreementCheckbox.checked
-    ) {
+  checkbox.addEventListener('change', (evt) => {
+    const closestForm = evt.target.closest('.form');
+    const agreementCheckbox = closestForm.querySelector('input[name=agreement]');
+    if (hasInvalidInputs(Array.from(closestForm.querySelectorAll('.form__input'))) || !agreementCheckbox.checked) {
       deactivateSubmitButton(closestForm);
     } else {
       activateSubmitButton(closestForm);
@@ -190,26 +179,26 @@ agreementCheckboxes.forEach((checkbox) => {
   });
 });
 smallMenuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
+  link.addEventListener('click', () => {
     unlockScroll();
-    smallMenuOverlay.classList.remove("nav-menu-small__overlay_active");
-    smallNavMenu.classList.remove("nav-menu-small_active");
+    smallMenuOverlay.classList.remove('nav-menu-small__overlay_active');
+    smallNavMenu.classList.remove('nav-menu-small_active');
   });
 });
 
 screen.orientation.addEventListener('change', () => {
-  scrollWidth = window.innerWidth - root.offsetWidth + "px";
-  slideWidth = document.querySelector(".our-production__slider-image").offsetWidth;
+  scrollWidth = window.innerWidth - root.offsetWidth + 'px';
+  slideWidth = document.querySelector('.our-production__slider-image').offsetWidth;
   sliderWrapper.style.transform = `translateX(0)`;
   slideWidthCounter = 0;
-})
+});
 
 // Функция, которая добавляет стиль для кнопки при прокрутке
 function scrollScreenToTop(scrollDistance) {
   if (window.scrollY >= scrollDistance) {
-    buttonToUp.style.display = 'flex'
+    buttonToUp.style.display = 'flex';
   } else {
-    buttonToUp.style.display = 'none'
+    buttonToUp.style.display = 'none';
   }
 }
 window.addEventListener('scroll', function () {
@@ -219,9 +208,19 @@ window.addEventListener('scroll', function () {
 function scrollToTop() {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: 'smooth',
   });
 }
-buttonToUp.addEventListener('click', scrollToTop)
+buttonToUp.addEventListener('click', scrollToTop);
+buttonCloseBanner.addEventListener('click', () => {
+  tinkoffBanner.style.display = 'none';
+  if (window.innerWidth > 529) {
+    navMenu.style.padding = '60px 15px 15px';
+  } else if (window.innerWidth <= 529 && window.innerWidth > 380) {
+    navMenu.style.padding = '90px 15px 15px';
+  } else {
+    navMenu.style.padding = '60px 15px 15px';
+  }
+});
 
 maskPhone('input[name="number"]');
